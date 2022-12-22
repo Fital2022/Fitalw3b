09d17fe306c49ddb975352829d3420e13f716cec
Brian <starly_balli@hotmail.com>
2022-12-22 12:59:49 -0600

    gus error fixed

--- pages/registro/index.tsx
+++ pages/registro/index.tsx
@@ -5,15 +5,11 @@ import * as React from 'react';
 
 import { Box, Button, Grid, Link } from '@mui/material';
 
-import { FirstLayout } from '../../components/Layout';
 import styles from './Welcome.module.css'
-
 
 const RegistroPage: NextPage = () => {
   return (
-    <FirstLayout title={'Fital'} pageDescription={'Inicio'}>
-      <h1>Registro</h1>
-    </FirstLayout >
+null
   )
 }
 
