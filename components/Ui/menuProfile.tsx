b39e2e4740d73ff1ffc06579e94c702e77e31b9c
Brian <starly_balli@hotmail.com>
2023-01-09 11:14:06 -0600

    gus error fixed

--- components/Ui/menuProfile.tsx
+++ components/Ui/menuProfile.tsx
@@ -18,7 +18,7 @@ import styles from "../../styles/Things.module.css";
 export const MenuProfile = () => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => { 
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
